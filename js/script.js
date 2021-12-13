a6a66273884093874406993c2a8a23ead95e4d5e
nandotezza <fernando.tezza@gmail.com>
2021-12-13 18:55:40 -0300

    Change the final date. Initiate a new counter

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@
 function runCountdown() {
     console.log(expired)
     if (!document.hidden) {
-        const date = new Date("December 12, 2021 20:50:00").getTime()    
+        const date = new Date("January 01, 2023 00:00:00").getTime()    
         const now = new Date().getTime()                
         const difference = date - now
 
